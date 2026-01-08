dafbf1af2c2ade6eb1729262244af4e6a1e06533
Piyush codes <boost2hire@gmail.com>
2026-01-08 23:03:34 +0530

    Remove backend and env from frontend repo

--- backend/codegen.py
+++
@@ -1,8 +0,0 @@
-import secrets
-import string
-
-def generate_code():
-    return "".join(
-        secrets.choice(string.ascii_uppercase + string.digits)
-        for _ in range(6)
-    )
--- backend/voice/beep.py
+++
@@ -1,7 +0,0 @@
-# voice/beep.py
-
-import winsound
-
-def play_beep():
-    # frequency (Hz), duration (ms)
-    winsound.Beep(750, 120)
